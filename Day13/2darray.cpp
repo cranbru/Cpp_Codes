967523186aeed44f5cd25097e8437e6b7fca55a5
cranbru <leejoksukisok@gmail.com>
2024-08-07 20:30:35 +0530

    2d array Day13

---
+++ Day13/2darray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void Printarr(int arr[][3] , int nrow , int ncolumn){// The size of dimensions of a multidimensional array must be specified
+    for(int i = 0 ; i<nrow ; i++){                   // To correctly compute the memory address of elements in the 2D array
+        for(int j = 0 ; j<ncolumn ; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void Inputarr(int arr[][3], int nrow , int ncolumn){
+    for(int i = 0; i<nrow ; i++){
+        for(int j =0 ; j<ncolumn ; j++){//Made a typo instead of j<ncolumn i wrote i<ncolumn #CAREFUL
+            cin>>arr[i][j];
+        }
+    }
+
+
+
+}
+
+int main(){
+    int arr[3][3] ;
+    int nrow = 3 , ncolumn = 3;
+    Inputarr(arr,nrow,ncolumn);
+    Printarr(arr,nrow,ncolumn);
+
+    return 0;
+}
