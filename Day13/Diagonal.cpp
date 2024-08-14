a9c04184a9f1adbca776394852f8b8c3599c97b4
cranbru <leejoksukisok@gmail.com>
2024-08-14 17:00:53 +0530

    Update Diagonal.cpp

--- Day13/Diagonal.cpp
+++ Day13/Diagonal.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
-
-void Diagon(int arr[][4],int n , int m){
+//YEH CODE N*N TC SE N TC TAK OPTIMIZE HO SAKTA HAI BAAD MAI CODE LIKH DENA ISKA
+void Diagon(int arr[][3],int n , int m){
     int sum = 0 ;
 
     for(int i = 0 ; i < n ; i++){
         sum+=arr[i][i];
-
+        for(int j = 0 ; j<m ;j++){
+            if(j==n-i-1 && j!=i){
+                sum+=arr[i][j];
+            }
+    }
     
     }
-    for(int j = 0 ; j<m ;j++){
-        if(j)
-    }
+    
     cout<<sum;
 
 
 }
 
 int main(){
-    int arr[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-    int n = 4 ; int m =4;
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int n = 3 ; int m =3;
     Diagon(arr,n,m);
     return 0;
 }
