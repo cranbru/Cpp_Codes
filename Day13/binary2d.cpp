6aabef4dc0583a53e73613a51ec6b180294a44b8
cranbru <leejoksukisok@gmail.com>
2024-08-15 13:07:48 +0530

    stair and binary

---
+++ Day13/binary2d.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void binary(int arr[][4],int n , int m , int key){
+    
+}
+
+
+int main(){
+     int arr[4][4] = {{1,2,3,4},
+                    {5,6,7,8},
+                    {9,10,11,12},
+                    {13,14,15,16}};
+
+    int n = 4 , m = 4 , key = 129;
+}
