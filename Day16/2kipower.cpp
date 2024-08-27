2c180ecd754973291896af8505cd4ef7878871a6
cranbru <leejoksukisok@gmail.com>
2024-08-27 18:47:17 +0530

    Create 2kipower.cpp

---
+++ Day16/2kipower.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string check2power(int num){
+    if((num&(num-1))){
+        return "NOPE";
+    }
+    else{
+        return "YES";
+    }
+}
+
+int main(){
+    cout<<check2power(32);
+}
