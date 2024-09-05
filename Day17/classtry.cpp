3a0df449b2d2e29838b4e33ec321da0898820802
cranbru <leejoksukisok@gmail.com>
2024-09-05 19:43:03 +0530

    Update classtry.cpp

--- Day17/classtry.cpp
+++ Day17/classtry.cpp
@@ -8,9 +8,18 @@ using namespace std;
 class Student{ //UPPERCASE FIRST LETTER IS A COMMON PRACTICE 
 public:
 
+    //PROPERTIES
+    string abc ; 
+    float gpa;
+
+    //METHODS   
+    void getpercentage(){
+        cout<<(gpa*10)<<endl;
+    }
 
 };// SEMICOLON IS IMP
 
 int main(){
-
+    Student s1;  //NOW S1 VARIABLE IS OF TYPE "STUDENT".... STUDENT--> CLASS.... s1-->OBJECT
+    cout<<sizeof(s1)<<endl;// 32 ... WHERE SOME OF IT IS STRING AND THE REST FLOAT AND ADDITIONAL DATA
 }
