1e2f7b777da2ecf340914d79c1ffcc54c75390dd
cranbru <leejoksukisok@gmail.com>
2024-09-06 20:48:19 +0530

    Create negativeisalsotrue.cpp

---
+++ Experiments/negativeisalsotrue.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+//NEGATIVE VALUES IS ALSO TRUE , MEANS ANYTHING EXPECT ZERO(0) IS TRUE 
+
+int main(){
+    if(-2){
+        cout<<"NEGATIVE VALUE IS ALSO WORKING "<<endl;
+    }
+}
