ae98965c648fbdf822ad13b4508ec4ed5754152f
cranbru <leejoksukisok@gmail.com>
2024-09-06 20:48:15 +0530

    Create ternary.cpp

---
+++ Experiments/ternary.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//TERNARY OPERATOR WILL PRINT THE SECOND IF THE CONDITION FAILS , REGARDLESS THE SECOND ONE IS TRUE OR NOT 
+
+int main(){
+    int a =2,b=2;
+    cout<<(a>b?"yes":"no");
+}
