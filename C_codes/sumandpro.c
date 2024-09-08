2e238bd1f5043cc199b12c4f68f7253e989e9263
cranbru <leejoksukisok@gmail.com>
2024-09-08 16:06:04 +0530

    Create sumandpro.c

---
+++ C_codes/sumandpro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int sumofints(int a , int b){
+    return(a+b);
+}
+
+float productoffloats(float a, float b){
+    return(a*b);
+}
+
+int main(){
+    printf("%d\n",sumofints(7,3)); // MISSED A \ AND WAS THINKING WHY MY CODE IS NOT WORKING :(
+    printf("%f",productoffloats(3.13,123.12));
+}
