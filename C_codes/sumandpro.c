8ea0e06fc9518cb6b02bc21460fec757d00a4cc5
cranbru <leejoksukisok@gmail.com>
2024-09-10 00:15:56 +0530

    Update sumandpro.c

--- C_codes/sumandpro.c
+++ C_codes/sumandpro.c
@@ -12,4 +12,4 @@ float productoffloats(float a, float b){
 int main(){
     printf("%d\n",sumofints(7,3)); // MISSED A \ AND WAS THINKING WHY MY CODE IS NOT WORKING :(
     printf("%f",productoffloats(3.13,123.12));
-}
+} 
