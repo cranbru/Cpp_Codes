8db676f4ec8060638c62477e436ba1ce78731644
cranbru <leejoksukisok@gmail.com>
2024-09-12 20:36:57 +0530

    Create fnoverinclass.cpp

---
+++ Day18/fnoverinclass.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//THIS IS COMPILE TIME OVERLOADING/POLYMORPHISM
+
+class Print{
+public:
+    void show(int digit){
+        cout<<"THE FUNC WITH INTEGER IS CALLED"<<endl;
+    }
+    void show(string quote){
+        cout<<"THE FUNC WITH STRING IS CALLED"<<endl;
+        
+    }
+};
+
+
+int main(){
+    Print p1;
+    p1.show("IDK");
+}
