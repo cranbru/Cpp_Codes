df734f148fbcb779856d68dfdc87db2026a29ba7
cranbru <leejoksukisok@gmail.com>
2024-09-12 22:17:50 +0530

    Create 1684.cpp

---
+++ LCQUES/1684.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        
+        for(int i = 0 ;i<n;i++){
+
+        }
+    }   
+};
+
+int main(){
+
+
+    return 0;
+}
