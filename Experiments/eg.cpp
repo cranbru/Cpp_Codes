d45290c5d11f127ac4c30108b46a548edc9c71a2
cranbru <leejoksukisok@gmail.com>
2024-09-12 20:37:01 +0530

    Create eg.cpp

---
+++ Experiments/eg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void idk(){
+    int a =10;
+    int b = 0;
+    int result =a/b;
+    cout<<"RESULT : "<<result<<endl;
+}
+
+
+int main()
+{   
+    idk();
+    return 0 ;
+}
