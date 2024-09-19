a111664e1100f1751efc88049b3342010f95ec14
cranbru <leejoksukisok@gmail.com>
2024-09-19 20:32:15 +0530

    Update binarywithrecursion.cpp

--- Day19/binarywithrecursion.cpp
+++ Day19/binarywithrecursion.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int Binarysearch(int arr[],int key,int start,int end    ){
+int Binarysearch(int arr[],int target,int start,int end){
+    int mid=start+(end-start)/2;
+
+    if(start>end){
+        return -1;
+    }
+
+    if(target==arr[mid]){
+        return(mid);
+    }
+
+    if(target>arr[mid]){
+        start=mid+1;
+    }
     
+    else{
+        end=mid-1;
+    }
+
+    return Binarysearch(arr,target,start,end);
 }
 
 int main(){
-
+    int arr1[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    int target = 13;
+    int n = sizeof(arr1)/sizeof(int);
+    cout<<Binarysearch(arr1,target,0,n-1)<<endl;
     return 0;
 }
