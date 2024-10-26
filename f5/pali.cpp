854b34fa5653e2a1d607947c66eacaf108dca654
cranbru <leejoksukisok@gmail.com>
2024-10-26 22:25:59 +0530

    idk

--- f5/pali.cpp
+++ f5/pali.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    string str ="";
-    cin>>str;
+    string str ="racecar";
+    //cin>>str;
     bool ispali = true;
     for(int i = 0 ;i<(str.size()/2);i++){
         if(str[i]!=str[str.size()-i-1]){
