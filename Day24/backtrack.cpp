260701313c57de932113c7162a6a5a2258c9c6b2
cranbru <leejoksukisok@gmail.com>
2024-11-04 13:16:53 +0530

    Create backtrack.cpp

---
+++ Day24/backtrack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Printvec(vector <int> vec){
+    for(int i = 0;i<vec.size();i++){
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+void func(vector <int> &vec,int i){
+    if(i==(vec.size())){
+        Printvec(vec);
+        return;
+    }
+    vec[i]=i+1;
+    func(vec,i+1);
+    vec[i]-=2;
+}
+
+
+
+int main() {
+    vector <int> vec(5);
+    func(vec,0);
+    Printvec(vec);
+    return 0;
+}
