1deaed895344707de9e4a5224d68ea1e7da5837a
cranbru <leejoksukisok@gmail.com>
2024-12-13 23:10:28 +0530

    Create mathlogic.cpp

---
+++ EvenOddchallenge/mathlogic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 7;
+
+    if((num/2)*2==num){
+        cout<<"NUMBER IS EVEN";
+    }
+    else{
+        cout<<"NUMBER IS ODD";
+    }
+    return 0;
+}
