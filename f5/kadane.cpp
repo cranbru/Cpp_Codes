e26d0e91f7132b4e31824784c1a29cc7634ad88a
cranbru <leejoksukisok@gmail.com>
2024-12-13 23:10:30 +0530

    Create kadane.cpp

---
+++ f5/kadane.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int kadane(vector <int> nums){
+
+    int maxsum = nums[0];   
+    for(int i =0;i<nums.size();i++){
+        if(nums[i]>0){
+                
+        }
+    }
+
+
+
+
+}
+
+
+
+
+int main() {
+    vector <int> nums ={-2,1,-3,4,-1,2,1,-5,4};
+    kadane(nums);
+    return 0;
+}
