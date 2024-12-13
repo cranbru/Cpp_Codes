30192bdba3b924abf91cfd57c4e7c9cf6d5d38fb
cranbru <leejoksukisok@gmail.com>
2024-12-13 23:10:35 +0530

    Create substring.cpp

---
+++ f5/substring.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void substr(string str,string ans = ""){
+    for(int i = 0;i<str.size();i++){
+                
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    string str = "abc";
+    substr(str);
+    return 0;
+}
